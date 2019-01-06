147f45c85eea945ddd02a7e2e94c8a9b22c6fa36
Kunzyy <44604068+Kunzyy@users.noreply.github.com>
2019-01-06 04:28:32 +0100

    Kuntz

--- src/main/QuantiteModif.java
+++ src/main/QuantiteModif.java
@@ -22,6 +22,8 @@ public class QuantiteModif extends JFrame {
         setPreferredSize(new Dimension(350,100));
         setMinimumSize(new Dimension(350,100));
 
+        spinner1.setModel(new SpinnerNumberModel(1, 1, 200, 1));
+
         String query ="INSERT INTO Modif ( idComPlat, Modif, idIngredient, AjoutRetrait, Quantite )" +
                 "VALUES (?, ?, ?, ?, ?);";
         ArrayList<String> tab = new ArrayList<>();
